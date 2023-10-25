4ccf7f6169ed6a736d0921fefb3a494d145844e9
paul.hornig <paul.hornig@ort-online.net>
2023-10-25 16:44:57 +0200

    Bubblesort started

---
+++ sorts/bubblesort/bubblesort.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Bubblesort
+{
+    class Bubblesort
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello World\n");
+        }
+
+    }
+}
---
+++ sorts/bubblesort/build/compile.sh
@@ -0,0 +1,5 @@
+printf "Compiling...\n"
+
+mcs ../bubblesort.cs -out:../../../executables/bubblesort.exe
+
+printf "Compiling finished!\n"
